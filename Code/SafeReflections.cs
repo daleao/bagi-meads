a293d99fc6fde6022d2e71a7b59794040664a549
Mat L <2775342-daleao@users.noreply.gitlab.com>
2023-09-24 16:16:52 +0000

    Renamed TypeExtensions

--- Code/SafeReflections.cs
+++ Code/SafeReflections.cs
@@ -7,7 +7,7 @@ namespace DaLion.Meads;
 
 /// <summary>Provides extensions for simplified reflection on C# types.</summary>
 /// <remarks>Credit to <c>Pardeike</c>.</remarks>
-public static class TypeExtensions
+public static class SafeReflections
 {
     /// <summary>Get a type by name and assert that it was found.</summary>
     /// <param name="name">The name of the type, preferably with namespace.</param>
